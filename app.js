75df42f7e6197bb1edb1746fcf0e7c7e385c6225
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-01-26 11:49:53 -0600

    adding package.json set up for tests

---
+++ app.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+//^ this line tells the command line that we are running in node / running a js file
+
+
+console.log("bruuuuh");
+
+
+
+
+
---
+++ tests/mainTest.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const {expect} = require("chai");
+
+
+describe("My test suite", () => {
+  
+  it("should equal true", () => {
+    expect(true).to.be.true;
+  });
+
+});
