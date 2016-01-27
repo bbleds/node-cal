10fd70d07f8b31258fda9a651637421469eb84e1
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-01-26 21:12:08 -0600

    added tests for header and body of calendar

--- tests/mainTest.js
+++ tests/mainTest.js
@@ -47,9 +47,17 @@ const { execSync } = require('child_process');
 
 });
 
-  describe("Console outputs", () => {
-    const { calBodyOutput, calHeaderOutput } = require("../lib/monthGen.js")
+  describe("Calendar console outputs", () => {
+    const { calBodyOutput, calHeaderOutput } = require("../lib/monthGen.js");
     it("should output the month and year centered relative to 20 spaces", ()=>{
+      expect(calHeaderOutput(3,2016)).to.equal("    March 2016\nSu Mo Tu We Th Fr Sa");
+    });
+
+    it("should output the days of the month relative to 20 spaces", ()=>{
+      expect(calBodyOutput(3, 2016)).to.equal("       1  2  3  4  5 \n 6  7  8  9 10 11 12 \n13 14 15 16 17 18 19 \n20 21 22 23 24 25 26 \n27 28 29 30 31")
+    });
+
+    it("should output a full calendar with a calendar head and a calendar body", () => {
 
     });
 
