41ff47f3011eaa5fb906c6d22c7cf53700426eb8
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-01-28 11:41:27 -0600

    added tests for weeks and days

--- tests/mainTest.js
+++ tests/mainTest.js
@@ -2,6 +2,7 @@
 
 const { expect } = require('chai');
 const { execSync } = require('child_process');
+  const { outputCal, calBodyOutput, calHeaderOutput, getWeeks, getTotalDays } = require("../lib/monthGen.js");
 
 describe('cal', () => {
   describe('CLI', () => {
@@ -16,21 +17,6 @@ describe('cal', () => {
 
 //test for weeks
 describe("Week tests", () => {
-  const { outputCal, calBodyOutput, calHeaderOutput } = require("../lib/monthGen.js");
-
-
-
-  const getWeeks = (month, year) => {
-        console.log(calBodyOutput(month, year).lastIndexOf("\n"));
-
-        if(calBodyOutput(month, year).lastIndexOf("\n") === 62){
-          return 4;
-        } else if (calBodyOutput(month, year).lastIndexOf("\n") === 83){
-          return 5
-        } else if (calBodyOutput(month, year).lastIndexOf("\n") === 104){
-          return 6
-        }
-  };
 
 
   it('should the retrn the appropriate number of weeks for all types of weeks', () => {
@@ -49,59 +35,48 @@ describe("Week tests", () => {
      expect(execSync('./lib/cal.js 0 1872').toString()).to.equal("Arguments are invalid\n");
      expect(execSync('./lib/cal.js 3 1672').toString()).to.equal("Arguments are invalid\n");
      expect(execSync('./lib/cal.js 3 0').toString()).to.equal("Arguments are invalid\n");
-
    });
-
 
 });
 
+//test for total days
+describe("day tests", () => {
+  it("should return the total number of days in the month", () => {
+  expect(getTotalDays(11,2015)).to.equal(30);
+  });
 
-//   describe("Zeller's congruence", () => {
-//      const zellers = require('../lib/zellars.js');
+});
 
-//     describe('.modifiedMonth', () => {
-//       it('return 13 for January', () => {
-//         expect(zellers.modifiedMonth(1)).to.equal(13);
-//         expect(zellers.modifiedMonth(2)).to.equal(14);
-//          expect(zellers.modifiedMonth(3)).to.equal(3);
+describe("Zeller's congruence", () => {
+     const zellers = require('../lib/zellars.js');
 
-//       });
-//     });
+    describe('.modifiedMonth', () => {
+      it('return 13 for January', () => {
+        expect(zellers.modifiedMonth(1)).to.equal(13);
+        expect(zellers.modifiedMonth(2)).to.equal(14);
+         expect(zellers.modifiedMonth(3)).to.equal(3);
 
-//     //describe('.modifiedYear', () => {
-//       it('returns 2015 for Jan 2015', () => {
-//         expect(zellers.modifiedYear(2015, 1)).to.equal(2014);
-//       });
-//       // 2016, 2 === 2015
-//       // 2017, 3 === 2017
+      });
+    });
 
-//     describe('.getDay', () => {
-//       it('returns 2 (Tuesday) for March 1, 2016', () => {
-//         expect(zellers.getDay(2000, 3, 1)).to.equal(3);
-//         expect(zellers.getDay(2100, 3, 1)).to.equal(1);
-//         expect(zellers.getDay(2200, 3, 2)).to.equal(0);
-//         expect(zellers.getDay(2300, 3, 1)).to.equal(4);
-//       });
-//       // 2000, 3, 1 === 3
-//       // 2100, 3, 1 === 1
-//       // 2200, 3, 2 === 0
-//       // 2300, 3, 1 === 4
-//     });
+    describe('.modifiedYear', () => {
+      it('returns 2015 for Jan 2015', () => {
+        expect(zellers.modifiedYear(2015, 1)).to.equal(2014);
+        expect(zellers.modifiedYear(2016, 2)).to.equal(2015);
+        expect(zellers.modifiedYear(2017, 3)).to.equal(2017);
+      });
 
-// });
+    describe('.getDay', () => {
+      it('returns 2 (Tuesday) for March 1, 2016', () => {
+        expect(zellers.getDay(2000, 3, 1)).to.equal(3);
+        expect(zellers.getDay(2100, 3, 1)).to.equal(1);
+        expect(zellers.getDay(2200, 3, 2)).to.equal(0);
+        expect(zellers.getDay(2300, 3, 1)).to.equal(4);
+      });
+    });
 
-//   describe("Calendar console outputs", () => {
-//     const { calBodyOutput, calHeaderOutput, outputCal } = require("../lib/monthGen.js");
+    });
+  });
 
-//     //   console.log(outputCal(3,2016));
-//     // it("should output the month and year centered relative to 20 spaces", ()=>{
-//     //   expect(calHeaderOutput(3,2016)).to.equal("    March 2016\nSu Mo Tu We Th Fr Sa");
-//     // });
-
-//     // it("should output the days of the month relative to 20 spaces", ()=>{
-//     //   expect(calBodyOutput(3, 2016)).to.equal("       1  2  3  4  5 \n 6  7  8  9 10 11 12 \n13 14 15 16 17 18 19 \n20 21 22 23 24 25 26 \n27 28 29 30 31")
-//     // });
-
-//   });
 
 
