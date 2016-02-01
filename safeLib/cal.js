9b7b9d92ba184d58a463bd4939c410a97d34d782
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-01 14:35:37 -0600

    transpiled to es5

---
+++ safeLib/cal.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+'use strict';
+
+//import monthGen
+
+const { outputCal } = require("./monthGen.js");
+const { makeYear } = require("./yearGen.js")
+
+// ignore first two items in argsv array and give me just the command line arguments
+const [,,...argv] = process.argv;
+
+
+let errorMsg = "Arguments are invalid"
+
+//get the month and year of today
+const month = new Date().getMonth() + 1;
+const year = new Date().getFullYear();
+
+
+//handle args
+//if there is one arg, check for year, if two check for year and month
+argv.length === 0 ? console.log(outputCal(month,year)) :
+argv.length === 1 && parseInt(argv[0]) > 1753 && parseInt(argv[0]) < 9999 ? makeYear(argv[0]) :
+argv.length === 2 && parseInt(argv[0]) > 0 && parseInt(argv[0]) <= 12 && parseInt(argv[1]) > 1753 && parseInt(argv[1]) <= 9999 ? console.log(outputCal(parseInt(argv[0]),parseInt(argv[1]))): console.log(errorMsg);
+
+
+
